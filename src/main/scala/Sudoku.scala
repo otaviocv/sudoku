e7e20e2fa63ffc46b172a15a60424d270e19fe74
Otávio Vasques <otaviocv.deluqui@gmail.com>
2019-05-09 22:51:56 -0300

    Fisrt check functions

--- src/main/scala/Sudoku.scala
+++ src/main/scala/Sudoku.scala
@@ -7,15 +7,18 @@ class Sudoku(val rawBoard: String) {
 
   def solved(board: Array[Array[Int]]): Boolean = {
 
-    def checkColumnSum(col: Int): Boolean = ???
+    def checkColumnSum(col: Int): Boolean = board.foldLeft(0)(_ + _(col))
     
     def checkColumnDuplicates(col: Int): Boolean = ???
 
-    def checkRowSum(row: Int): Boolean = ???
+    def checkRowSum(row: Int): Boolean = board(row).sum
 
     def checkRowDuplicates(row: Int): Boolean = ???
 
-    def checkBoxSum(row: Int, col: Int): Boolean = ???
+    def checkBoxSum(row: Int, col: Int): Boolean = {
+      val row_i: Int = row % 3
+      val col_i: Int = col % 3
+    }
 
     def checkBoxDuplicates(rowBox: Int, colBox: Int): Boolean = ???
 
